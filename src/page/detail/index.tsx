39a79ffa6aba459e23cd77c85d13f49bf6691105
Kim Ji Yu <96280450+Banal972@users.noreply.github.com>
2025-09-19 04:28:31 +0900

    feat 상세 스켈레톤

--- src/page/detail/index.tsx
+++ src/page/detail/index.tsx
@@ -1,6 +1,5 @@
-import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import {
   ChartContainer,
   ChartLegend,
@@ -9,13 +8,6 @@ import {
   ChartTooltipContent,
   type ChartConfig,
 } from "@/components/ui/chart";
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 import { Separator } from "@/components/ui/separator";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
@@ -24,13 +16,12 @@ import {
   type PoolHistoryChart,
   type PoolHistoryType,
 } from "@/hooks/api/pool";
-import { DialogDescription } from "@radix-ui/react-dialog";
-import { ChevronRight } from "lucide-react";
 import { useEffect, useState, type ChangeEvent } from "react";
 import { useParams } from "react-router";
 import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
 import { toast } from "sonner";
 import dayjs from "dayjs";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface MergedHistoryRowType {
   timestamp: string;
@@ -118,211 +109,221 @@ const DetailPage = () => {
     );
   };
 
-  if (poolDataPending || poolHistoryPending) return null; // 로딩화면 혹은 스켈레톤
+  const isPending = poolDataPending || poolHistoryPending;
+
   if (poolDataStatus === "error" || poolHistoryStatus === "error") return null; // 에러 화면
 
   return (
     <div className="flex gap-8 relative items-start">
       <div className="flex-2 space-y-16">
         <Card>
-          <ChartContainer config={chartConfig}>
-            <AreaChart data={mergeHistory(poolHistory)}>
-              <defs>
-                <linearGradient id="funding_apr" x1="0" y1="0" x2="0" y2="1">
-                  <stop
-                    offset="5%"
-                    stopColor="var(--color-desktop)"
-                    stopOpacity={0.8}
+          <CardContent>
+            {isPending && <Skeleton className="h-[400px] rounded-3xl w-full" />}
+            {!isPending && (
+              <ChartContainer config={chartConfig} className="h-[400px] w-full">
+                <AreaChart data={mergeHistory(poolHistory)}>
+                  <defs>
+                    <linearGradient
+                      id="funding_apr"
+                      x1="0"
+                      y1="0"
+                      x2="0"
+                      y2="1"
+                    >
+                      <stop
+                        offset="5%"
+                        stopColor="var(--color-desktop)"
+                        stopOpacity={0.8}
+                      />
+                      <stop
+                        offset="95%"
+                        stopColor="var(--color-desktop)"
+                        stopOpacity={0.1}
+                      />
+                    </linearGradient>
+                    <linearGradient
+                      id="combined_apr"
+                      x1="0"
+                      y1="0"
+                      x2="0"
+                      y2="1"
+                    >
+                      <stop
+                        offset="5%"
+                        stopColor="var(--color-mobile)"
+                        stopOpacity={0.8}
+                      />
+                      <stop
+                        offset="95%"
+                        stopColor="var(--color-mobile)"
+                        stopOpacity={0.1}
+                      />
+                    </linearGradient>
+                    <linearGradient
+                      id="effective_apr"
+                      x1="0"
+                      y1="0"
+                      x2="0"
+                      y2="1"
+                    >
+                      <stop
+                        offset="5%"
+                        stopColor="var(--color-desktop)"
+                        stopOpacity={0.8}
+                      />
+                      <stop
+                        offset="95%"
+                        stopColor="var(--color-desktop)"
+                        stopOpacity={0.1}
+                      />
+                    </linearGradient>
+                  </defs>
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis
+                    dataKey="timestamp"
+                    tickLine={false}
+                    axisLine={false}
+                    tickMargin={8}
+                    minTickGap={32}
+                    tickFormatter={(value) =>
+                      dayjs(value).format("MMM D, HH:mm")
+                    }
                   />
-                  <stop
-                    offset="95%"
-                    stopColor="var(--color-desktop)"
-                    stopOpacity={0.1}
+                  <ChartTooltip
+                    cursor={false}
+                    content={<ChartTooltipContent />}
                   />
-                </linearGradient>
-                <linearGradient id="combined_apr" x1="0" y1="0" x2="0" y2="1">
-                  <stop
-                    offset="5%"
-                    stopColor="var(--color-mobile)"
-                    stopOpacity={0.8}
+                  <Area
+                    dataKey="funding_apr"
+                    type="natural"
+                    fill="url(#funding_apr)"
+                    stroke="var(--chart-1)"
                   />
-                  <stop
-                    offset="95%"
-                    stopColor="var(--color-mobile)"
-                    stopOpacity={0.1}
+                  <Area
+                    dataKey="combined_apr"
+                    type="natural"
+                    fill="url(#combined_apr)"
+                    stroke="var(--chart-2)"
                   />
-                </linearGradient>
-                <linearGradient id="effective_apr" x1="0" y1="0" x2="0" y2="1">
-                  <stop
-                    offset="5%"
-                    stopColor="var(--color-desktop)"
-                    stopOpacity={0.8}
+                  <Area
+                    dataKey="effective_apr"
+                    type="natural"
+                    fill="url(#effective_apr)"
+                    stroke="var(--chart-3)"
                   />
-                  <stop
-                    offset="95%"
-                    stopColor="var(--color-desktop)"
-                    stopOpacity={0.1}
-                  />
-                </linearGradient>
-              </defs>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis
-                dataKey="timestamp"
-                tickLine={false}
-                axisLine={false}
-                tickMargin={8}
-                minTickGap={32}
-                tickFormatter={(value) => dayjs(value).format("MMM D, HH:mm")}
-              />
-              <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
-              <Area
-                dataKey="funding_apr"
-                type="natural"
-                fill="url(#funding_apr)"
-                stroke="var(--chart-1)"
-              />
-              <Area
-                dataKey="combined_apr"
-                type="natural"
-                fill="url(#combined_apr)"
-                stroke="var(--chart-2)"
-              />
-              <Area
-                dataKey="effective_apr"
-                type="natural"
-                fill="url(#effective_apr)"
-                stroke="var(--chart-3)"
-              />
-              <ChartLegend content={<ChartLegendContent />} />
-            </AreaChart>
-          </ChartContainer>
+                  <ChartLegend content={<ChartLegendContent />} />
+                </AreaChart>
+              </ChartContainer>
+            )}
+          </CardContent>
         </Card>
 
         <Separator />
 
         <div className="grid gap-5">
-          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-            Position
-          </h3>
-          <div className="grid grid-cols-2 gap-2">
-            <Card></Card>
-            <Card></Card>
-          </div>
+          <Tabs defaultValue="position">
+            <TabsList className="bg-transparent">
+              <TabsTrigger
+                value="position"
+                className="scroll-m-20 text-2xl font-semibold tracking-tight border-0 data-[state=active]:bg-transparent! cursor-pointer"
+              >
+                Position
+              </TabsTrigger>
+              <Separator orientation="vertical" />
+              <TabsTrigger
+                value="transaction"
+                className="scroll-m-20 text-2xl font-semibold tracking-tight border-0 data-[state=active]:bg-transparent! cursor-pointer"
+              >
+                Transaction
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="position">
+              <Separator orientation="vertical" />
+              <Separator orientation="vertical" />
+            </TabsContent>
+            <TabsContent value="transaction">
+              Change your password here.
+            </TabsContent>
+          </Tabs>
         </div>
       </div>
 
       <div className="flex-1 max-w-sm sticky top-18">
-        <Tabs defaultValue="buy" className="w-[400px]">
-          <TabsList className="bg-transparent">
-            <TabsTrigger value="buy" className="cursor-pointer">
-              Buy
-            </TabsTrigger>
-            <TabsTrigger value="sell" className="cursor-pointer">
-              Sell
-            </TabsTrigger>
-          </TabsList>
+        {isPending && (
+          <>
+            <Skeleton className="w-[100px] h-[30px]" />
+            <Skeleton className="w-full h-[220px] mt-5" />
+          </>
+        )}
+        {!isPending && (
+          <Tabs defaultValue="buy" className="w-[400px]">
+            <TabsList className="bg-transparent">
+              <TabsTrigger value="buy" className="cursor-pointer">
+                Buy
+              </TabsTrigger>
+              <TabsTrigger value="sell" className="cursor-pointer">
+                Sell
+              </TabsTrigger>
+            </TabsList>
 
-          <TabsContent value="buy" className="space-y-5">
-            <div className="text-4xl border rounded-xl py-12 px-4 flex flex-col items-center">
-              <div className="flex w-full justify-center items-center gap-1 min-w-0">
-                <p className="flex-none">US$</p>
-                <input
-                  value={val}
-                  onChange={onChange}
-                  inputMode="numeric"
-                  pattern="[0-9]*"
-                  className="outline-0 bg-transparent w-6"
-                  style={{ width: `${Math.max(1, val.length) * 24}px` }}
-                />
-              </div>
-              <div className="flex gap-2 items-center mt-8">
-                <Button variant="outline" onClick={() => setVal("100")}>
-                  $100
-                </Button>
-                <Button variant="outline" onClick={() => setVal("300")}>
-                  $300
-                </Button>
-                <Button variant="outline" onClick={() => setVal("1000")}>
-                  $1000
-                </Button>
-              </div>
-            </div>
-
-            <Dialog>
-              <DialogTrigger className="w-full">
-                <div className="p-4 border rounded-xl bg-neutral-900 cursor-pointer">
-                  <div className="flex justify-between items-center">
-                    <div className="flex items-center gap-2">
-                      <Avatar>
-                        <AvatarImage src="https://github.com/shadcn.png" />
-                      </Avatar>
-                      <p>Token Name</p>
-                    </div>
-                    <ChevronRight />
-                  </div>
+            <TabsContent value="buy" className="space-y-5">
+              <div className="text-4xl border rounded-xl py-12 px-4 flex flex-col items-center">
+                <div className="flex w-full justify-center items-center gap-1 min-w-0">
+                  <p className="flex-none">US$</p>
+                  <input
+                    value={val}
+                    onChange={onChange}
+                    inputMode="numeric"
+                    pattern="[0-9]*"
+                    className="outline-0 bg-transparent w-6"
+                    style={{ width: `${Math.max(1, val.length) * 24}px` }}
+                  />
                 </div>
-              </DialogTrigger>
-              <DialogContent>
-                <DialogHeader>
-                  <DialogTitle>Select Token</DialogTitle>
-                  <DialogDescription>Token List</DialogDescription>
-                </DialogHeader>
-              </DialogContent>
-            </Dialog>
-          </TabsContent>
-
-          <TabsContent value="sell" className="space-y-5">
-            <div className="text-4xl border rounded-xl py-12 px-4 flex flex-col items-center">
-              <div className="flex w-full justify-center items-center gap-1 min-w-0">
-                <p className="flex-none">US$</p>
-                <input
-                  value={val}
-                  onChange={onChange}
-                  inputMode="numeric"
-                  pattern="[0-9]*"
-                  className="outline-0 bg-transparent w-6"
-                  style={{ width: `${Math.max(1, val.length) * 24}px` }}
-                />
-              </div>
-              <div className="flex gap-2 items-center mt-8">
-                <Button variant="outline" onClick={() => setVal("100")}>
-                  25%
-                </Button>
-                <Button variant="outline" onClick={() => setVal("300")}>
-                  50%
-                </Button>
-                <Button variant="outline" onClick={() => setVal("1000")}>
-                  75%
-                </Button>
-                <Button variant="outline" onClick={() => setVal("1000")}>
-                  100%
-                </Button>
-              </div>
-            </div>
-
-            <Dialog>
-              <DialogTrigger className="w-full">
-                <div className="p-4 border rounded-xl bg-neutral-900 cursor-pointer">
-                  <div className="flex justify-between items-center">
-                    <div className="flex items-center gap-2">
-                      <Avatar>
-                        <AvatarImage src="https://github.com/shadcn.png" />
-                      </Avatar>
-                      <p>Token Name</p>
-                    </div>
-                    <ChevronRight />
-                  </div>
+                <div className="flex gap-2 items-center mt-8">
+                  <Button variant="outline" onClick={() => setVal("100")}>
+                    $100
+                  </Button>
+                  <Button variant="outline" onClick={() => setVal("300")}>
+                    $300
+                  </Button>
+                  <Button variant="outline" onClick={() => setVal("1000")}>
+                    $1000
+                  </Button>
                 </div>
-              </DialogTrigger>
-              <DialogContent>
-                <DialogHeader>
-                  <DialogTitle>Select Token</DialogTitle>
-                  <DialogDescription>Token List</DialogDescription>
-                </DialogHeader>
-              </DialogContent>
-            </Dialog>
-          </TabsContent>
-        </Tabs>
+              </div>
+            </TabsContent>
+
+            <TabsContent value="sell" className="space-y-5">
+              <div className="text-4xl border rounded-xl py-12 px-4 flex flex-col items-center">
+                <div className="flex w-full justify-center items-center gap-1 min-w-0">
+                  <p className="flex-none">US$</p>
+                  <input
+                    value={val}
+                    onChange={onChange}
+                    inputMode="numeric"
+                    pattern="[0-9]*"
+                    className="outline-0 bg-transparent w-6"
+                    style={{ width: `${Math.max(1, val.length) * 24}px` }}
+                  />
+                </div>
+                <div className="flex gap-2 items-center mt-8">
+                  <Button variant="outline" onClick={() => setVal("100")}>
+                    25%
+                  </Button>
+                  <Button variant="outline" onClick={() => setVal("300")}>
+                    50%
+                  </Button>
+                  <Button variant="outline" onClick={() => setVal("1000")}>
+                    75%
+                  </Button>
+                  <Button variant="outline" onClick={() => setVal("1000")}>
+                    100%
+                  </Button>
+                </div>
+              </div>
+            </TabsContent>
+          </Tabs>
+        )}
       </div>
     </div>
   );
