e80b477d64588655544d1ec194e6176670ac35f6
Kim Ji Yu <96280450+Banal972@users.noreply.github.com>
2025-09-20 21:58:21 +0900

    chore StrictMode remove

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import "@turnkey/react-wallet-kit/styles.css";
 import "./index.css";
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+createRoot(document.getElementById("root")!).render(<App />);
+
+{
+  /* <StrictMode>
+  </StrictMode>, */
+}
